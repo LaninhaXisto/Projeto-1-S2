0bcab9e60f9d4f09a8387955cf94bfbe95d9d2e9
laninha <148967264+LaninhaXisto@users.noreply.github.com>
2023-11-01 08:06:31 -0300

    adicionando arquivos html, css, js

---
+++ sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+    createCanvas(600, 600);
+    background( " hotpink");
+  }
+  
+  function draw() {
+  
+    
+    fill ('pink');
+    stroke ("black");
+    
+    if(mouseIsPressed) {
+    rect (mouseX, mouseY, 30, 30);  
+    }
+  }
+  
